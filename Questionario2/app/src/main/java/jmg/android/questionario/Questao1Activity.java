649da3261864c3d641e2919bd85965b306e5ed76
Jhone Mendonça <g.m.enohj@gmail.com>
2016-09-26 14:53:26 -0300

    questões

---
+++ Questionario2/app/src/main/java/jmg/android/questionario/Questao1Activity.java
@@ -0,0 +1,17 @@
+package jmg.android.questionario;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by aluno on 26/09/2016.
+ */
+
+public class Questao1Activity extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.questao1);
+    }
+}
